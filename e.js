9a41797a697dbcba9cbf4d1e9d27f09c59678f07
mattr <mattr@example.com>
2026-03-06 00:51:43 +0600

    add

--- e.js
+++
@@ -1,9 +0,0 @@
-const res = await fetch("http://localhost:3333/scrape", {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({ url: "https://www.ycombinator.com/companies/martini" })
-});
-
-const data = await res.json();
-console.log(data.text);
-console.log(`ms: ${data.ms}`);
